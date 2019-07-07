cc6618a6610a2916d4ab3c314cda5a48610be195
yarubfr <51924000+yarubfr@users.noreply.github.com>
2019-07-07 13:33:49 +0200

    pagina docs
    07072019-13:33

--- Asociacion/documentos.php
+++ Asociacion/documentos.php
@@ -17,194 +17,97 @@ include('../models/connection.php');
   
 ?>
 
-       
-  
+		<?php
+            if (isset($_SESSION['rol1']) !==  1 )
+				{
+				?>						
+
+							<div class="accordion" id="accordionExample">
+								  <div class="card">
+									<div class="card-header" id="headingOne">
+									  <h2 class="mb-0">
+										<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+										  Registro / Solicitudes
+										</button>
+									  </h2>
+									</div>
+
+									<div id="collapseOne" class="collapse show container" aria-labelledby="headingOne" data-parent="#accordionExample">
+										<div class="card-body ">
+											<div class="container">
+														<script src="https://www.w3schools.com/lib/w3.js"></script>
+														<table id="myTable" class="table">
+															<tr class="thead-light">
+																<th onclick="w3.sortHTML('#myTable', '.item', 'td:nth-child(1)')" style="cursor:pointer" >Título</th>
+																<th onclick="w3.sortHTML('#myTable', '.item', 'td:nth-child(2)')" style="cursor:pointer" >Descripción</th>
+																<th onclick="w3.sortHTML('#myTable', '.item', 'td:nth-child(3)')" style="cursor:pointer" >Subido por</th>
+																<th onclick="w3.sortHTML('#myTable', '.item', 'td:nth-child(4)')" style="cursor:pointer" >Fecha</th>
+																<th onclick="w3.sortHTML('#myTable', '.item', 'td:nth-child(5)')" style="cursor:pointer" >Enlace</th>
+															</tr>
+															<tbody>
+															
+																<?php
+																	$date = date("Y-m-d");
+																	$conexion = mysqli_connect('localhost', 'socio', 'socio', 'marte');
+																	$sql = "SELECT * FROM documentos, usuarios
+																			WHERE userid =id_usuario AND creation_date <= CURDATE()
+																			ORDER BY creation_date DESC";
+																	$consulta = mysqli_query($conexion, $sql);
+																	
+																	$result = $conexion->query($sql);
+																	if ($result->num_rows > 0)
+																		{
+																			while($row = $result->fetch_assoc())
+																				{?>
+																					<tr class="item">
+																						<td><?php echo $row['titulo']; ?></td>
+																						<td><?php echo $row['descripcion']; ?></td>
+																						<td><?php echo $row['usuario']; ?></td>
+																						<td><?php echo $row['creation_date']; ?></td>
+																						<?php
+																						$archivo = $row["documento"];
+																						$extension = explode(".", $archivo);
+																						$tipo = $extension[count($extension)-1];
+																						?>
+																						<td><a href=<?php echo $row["documento"]; ?>>Enlace</a> - <?php echo $tipo; ?></td>
+																					</tr>
+																				<?php
+																				}
+																				
+																		}
+																		else
+																		
+																		{
+																			echo "No hay documentos que mostrar";
+																		}
+																?>
 
 
-              <?php
+															</tbody>
+														</table>
+												  
+												  
+													
+											</div>
+										</div>
+									</div>
+								</div>  
+							</div>    
 
-              if( !isset($_SESSION['rol1']) || $_SESSION['rol1'] == 1)
-              {
-                echo(' <div class="alert alert-danger" role="alert">
-                    Sección disponible sólo para Socios !
-                  </div> ');
-              }
-              else
-                echo(' <div class="accordion" id="accordionExample">
-              <div class="card">
-                <div class="card-header" id="headingOne">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      Registro / Solicitudes
-                    </button>
-                  </h2>
-                </div>
+			<?php
+				}
+			else
+				{
+					echo ("No tienes permiso para visualizar el contenido");
+				}		
+					  
+							  
+							  
 
-                <div id="collapseOne" class="collapse show container" aria-labelledby="headingOne" data-parent="#accordionExample">
-                  <div class="card-body ">
-                      <div class="container">
-                      
-                          <table class="table">
-                            <thead class="thead-light">
-                              <tr>
-                                <th scope="col">Cod</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Tipo</th>
-                                <th scope="col">Fecha</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                              </tr>
-                               <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                         
-                            </tbody>
-                          </table>
-                      
-                        </div>
-
-                  </div>
-                </div>
-              </div>
-
-              <div class="card">
-                <div class="card-header" id="headingTwo">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                     Manuales / Doc. en pdf
-                    </button>
-                  </h2>
-                </div>
-                <div id="collapseTwo" class="collapse container" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                  <div class="card-body">
-                   <div class="container">
-                      
-                          <table class="table">
-                            <thead class="thead-light">
-                              <tr>
-                                <th scope="col">Cod</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Tipo</th>
-                                <th scope="col">Fecha</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                              </tr>
-                               <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                         
-                            </tbody>
-                          </table>
-                      
-                        </div>
-                  </div>
-                </div>
-              </div>
-                <div class="card">
-                <div class="card-header" id="headingThree">
-                  <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Reservas
-                    </button>
-                  </h2>
-                </div>
-                
-                <div id="collapseThree" class="collapse container" aria-labelledby="headingThree" data-parent="#accordionExample">
-                  <div class="card-body">
-                   <div class="container">
-                      
-                          <table class="table">
-                            <thead class="thead-light">
-                              <tr>
-                                <th scope="col">Cod</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Tipo</th>
-                                <th scope="col">Fecha</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                              </tr>
-                               <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                         
-                            </tbody>
-                          </table>
-                      
-                        </div>
-                  </div>
-                </div>
-                ');
-              ?>
-     
-           
-            
-
- 
    
 
-<?php
+
   include ('footer.php');
 ?>
+
+
