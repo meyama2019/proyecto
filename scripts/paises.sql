ba4221b2ffc055ae790587eea7a37dc9aec874a1
yarubfr <51924000+yarubfr@users.noreply.github.com>
2019-07-11 22:10:46 +0200

    tablas
    11072019

--- scripts/paises.sql
+++ scripts/paises.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-07-2019 a las 21:00:55
+-- Tiempo de generación: 11-07-2019 a las 22:10:22
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.4
 
