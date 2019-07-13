30fb43c518bc514d254d1075da05c6ff9348a570
elisaruiz <elisaruiza@gmail.com>
2019-07-13 19:37:19 +0200

    cambio del campo provincia

--- Asociacion/asociados.php
+++ Asociacion/asociados.php
@@ -15,7 +15,7 @@ include('../models/connection.php');
     $sql=$db->query(' SELECT id_usuario,usuario,email,Nom_Ape,dni,pr.provincia,pa.nombre,telefono,cuenta,activo,rol_id 
       FROM usuarios us  
       inner join paises pa on us.Pais = pa.id
-      inner join provincias pr on pr.id_provincia = us.provincia      
+      inner join provincias pr on pr.id_provincia = us.provincias      
       where us.rol_id = 2');
 
     // carga en la $listaUsuarios cada registro desde la base de datos
