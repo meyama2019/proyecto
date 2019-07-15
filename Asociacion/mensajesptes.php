dd3e1cd9b0522ba926b9e3b9d39d8c58c1069189
elisaruiz <elisaruiza@gmail.com>
2019-07-15 21:32:21 +0200

    actualiza contador mensajes pendientes

--- Asociacion/mensajesptes.php
+++ Asociacion/mensajesptes.php
@@ -18,6 +18,13 @@ include('../models/connection.php');
                   $tot=$tot+1;
                 }
                 $_SESSION['tot_pen'] = $tot;
+
+    $num_rows=$db->query('SELECT * FROM contacto where activo=1'); // 1 Pendientes aprobación
+    $tot=0;
+    foreach ($num_rows->fetchAll() as $contacto) {
+      $tot=$tot+1;
+    }
+    $_SESSION['tot_con'] = $tot;
               
 
     require_once('menu.php');
@@ -62,6 +69,14 @@ include('../models/connection.php');
                 }
                 $_SESSION['tot_con'] = $tot;
                 mysqli_close($conexion);
+
+                echo "<script type=\"text/javascript\">";
+                echo "$(document).ready(function(){";
+                echo "if(document.URL.indexOf(\"#\")==-1){";
+                echo " url = document.URL+\"#\"; location = \"#\";";
+                echo "location.reload(true);";
+                echo "}    });";
+                echo "</script>";
                             
           }
                 
