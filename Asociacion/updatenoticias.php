c6522faad1a9def22b11116139ddf129410436fb
elisaruiz <elisaruiza@gmail.com>
2019-07-25 20:44:38 +0200

    prueba acentos

--- Asociacion/updatenoticias.php
+++ Asociacion/updatenoticias.php
@@ -36,6 +36,7 @@ require_once('menu.php');
       //$conexion = mysqli_connect('localhost', 'socio', 'socio', 'marte');
       $acentos="SET NAMES 'utf8'";
       mysqli_query($conexion, $acentos);
+      mysqli_set_charset($conexion, 'utf8');
     
       $update = "UPDATE NOTICIAS set 
             userid = '$iduser',
