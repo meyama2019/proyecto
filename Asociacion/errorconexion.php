9bb1401ee09dd77d9e49a5d9e45e6d4c5930d9d6
marcelo <marcelo.canadaslopez@gmail.com>
2019-09-08 11:56:58 +0200

    08.09.2019 mcl Control error BBDD

---
+++ Asociacion/errorconexion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Trabajo - Modulo 3</title>
+    <script type="text/javascript" src="http://localhost/proyecto/js/jquery.js"></script>
+    <script type="text/javascript" src="http://localhost/proyecto/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://localhost/proyecto/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/proyecto/css/bootstrap.css">
+    <link rel="stylesheet" media="screen" type="text/css" href="http://localhost/proyecto/css/principal.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="http://localhost/proyecto/css/imprimir.css" />
+
+    <script type="text/javascript" src="../js/inicio.js"></script>
+</head>
+
+
+
+
+
+
+<body>
+  
+  <div class="container"> 
+    <div class="row">
+     <div class="col-sm-3">
+       
+     </div>
+     <div class="col-sm-6">
+      <div class="card text-center">
+            <div class="card-header">
+              Acceso a Datos
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">No se puede acceder a la BBDD</h5>
+              <p class="card-text">Contacte con Administrador</p>
+              <a href="../home.php" class="btn btn-primary">Entrar</a>
+            </div>
+            
+      </div>
+    </div>
+    <div class="col-sm-3">
+       
+     </div>
+  </div>
+  </div>
+
+
+</body>
+</html>
--- models/connection1.php
+++ models/connection1.php
@@ -1,7 +1,17 @@
 <?php
+	
+	 @$conexion = mysqli_connect('localhost', 'socio', 'socio', 'marte');
+	 if(!$conexion) 
+	 {
+	 	 @header("Location:http://localhost/proyecto/asociacion/errorconexion.php");
+	 }
+	 else
+	 {
+	 	$acentos="SET NAMES 'utf8'";
+     	mysqli_query($conexion, $acentos);
+	 }
+     
+     
 
-	 $conexion = mysqli_connect('localhost', 'socio', 'socio', 'marte');
-     $acentos="SET NAMES 'utf8'";
-     mysqli_query($conexion, $acentos);
 
 ?>
